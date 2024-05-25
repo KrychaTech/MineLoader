fcf1f04654d05df73c5e3a93fcd4e4894ae948fc
Krystian Antonkiewicz <83059152+KrychaTech@users.noreply.github.com>
2024-05-25 12:17:13 +0200

    Add files via upload

--- minecraft_runner.py
+++ minecraft_runner.py
@@ -1,7 +1,10 @@
 # python pico runner
-
+import argparse
 from portablemc.standard import Version
 
-version = Version("release")
+parser = argparse.ArgumentParser()
+parser.add_argument("-r", "--release", type=str, default="release", help="Release of minecraft you want to play. Defaults to latest.")
+args = parser.parse_args()
 
+version = Version(args.release)
 version.install().run()
